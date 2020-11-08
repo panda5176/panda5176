8f9baf16f3792416d5cbbe25229be2f957d2a54c
physcopatens12 <physcopatens12@gmail.com>
2020-11-08 19:06:46 +0900

    20-11-08-19-06.

--- etc/rosalind_alg.py
+++ etc/rosalind_alg.py
@@ -11,6 +11,41 @@ def fibo(n):
         return recurrence(n-1, Fn_1, Fn)
     return recurrence(n-1)
 
-def bind():
+def read_lbsv(lbsv):
+    """Reading line-break-seperated values file"""
+    values = []
+    with open(lbsv, 'r') as f: values = f.read().strip().split('\n')
+    return values
+
+def write_lbsv(values):
+    """Writing line-break-seperated values file"""
+    lbsv = "o.lbsv"
+    with open(lbsv, 'w') as f:
+        for value in values: f.write(str(value) + '\n')
+    return None
+
+def bind(lbsv):
     """Binary Search"""
-    pass
+    values = read_lbsv(lbsv)
+    n, _m, A, B = int(values[0]), int(values[1]), \
+        list(map(int, values[2].split())), list(map(int, values[3].split()))
+    idxes = []
+    def binary_search(b, n, A, origin = 0):
+        idx = n//2
+        if n <= 2:
+            if b == A[0]: return origin+1
+            elif n == 2 and b == A[1]: return origin+2
+            else: return -1
+        if b < A[idx]:
+            idx = binary_search(b, len(A[:idx]), A[:idx], origin)
+        elif b > A[idx]:
+            idx = binary_search(b, len(A[idx:]), A[idx:], origin + idx)
+        else:
+            return origin + idx + 1
+        return idx
+    for b in B: idxes.append(binary_search(b, n, A))
+    return idxes
+
+def deg(lbsv):
+    """Degree Array"""
+    edge_lists = read_lbsv(lbsv)
